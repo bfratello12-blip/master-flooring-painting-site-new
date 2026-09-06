a00c32e0536909065ba61b492323123d0505f3c3
bfratello12-blip <bfratello12@gmail.com>
2026-09-06 11:53:47 -0400

    changes

--- build.js
+++ build.js
@@ -475,6 +475,17 @@ function layout(meta, body) {
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YEGWGZ6R3Z"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YEGWGZ6R3Z');
+</script>
+
 <title>${meta.title}</title>
 <meta name="description" content="${meta.description}">
 <link rel="canonical" href="${url}">
